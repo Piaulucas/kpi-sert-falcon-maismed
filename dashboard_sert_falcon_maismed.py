b1672005d4208caee71c38e86c4bb242cd40f5ec
Piaulucas <piaulucas@gmail.com>
2026-05-29 18:31:07 -0300

    fix fillcolor rgba

--- dashboard_sert_falcon_maismed.py
+++ dashboard_sert_falcon_maismed.py
@@ -174,7 +174,7 @@ for chave, info in EMPRESAS.items():
                 line=dict(color=info['cor'], width=2),
                 marker=dict(size=5),
                 fill='tozeroy',
-                fillcolor=info['cor'] + '22',
+                fillcolor=info['cor'].replace('#','rgba(').rstrip(')') + ',0.15)',
             ))
             fig_f.update_layout(
                 plot_bgcolor='white', paper_bgcolor='white',
